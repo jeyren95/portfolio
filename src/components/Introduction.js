ba0c8a362c772fe3e1b7dcd4a67347968047984b
Poon Jey-ren <jeyren95@hotmail.com>
2021-05-01 13:15:04 +0800

    update media queries

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -12,7 +12,7 @@ class Introduction extends React.Component {
             <img src={logo} alt="logo-img"/>
           </a>
 
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
